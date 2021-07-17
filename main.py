a21b94aa1319b38babe8b7b978cbaa557a52332d
Andrew R <goggles@Andrews-MacBook-Pro.local>
2021-07-16 23:29:31 -0700

    Add comments to code

--- main.py
+++ main.py
@@ -2,14 +2,19 @@ import pandas as pd
 import geopandas as gpd
 import matplotlib.pyplot as plt
 
-
+# Load New Zealand coordinates into Geopandas
 map_df = gpd.read_file("data/nz-police-district-boundaries.shx")
+
+# Load firearm license data into Pandas
 licenses_df = pd.read_csv("data/nz_firearm_licenses.csv")
 
+# Combine both dataframes along their district columns
 merged_df = map_df.merge(licenses_df, left_on=["DISTRICT_N"], right_on=["Residence District"])
 
+# Create a Matplotlib plot/map with the combined dataframe. Use the "Licenses" column to decide colors.
 merged_df.plot(column="Licenses", cmap="Blues", legend=True)
 
+# Change the title and format of the Matplotlib plot/map
 plt.title("Firearm Licenses by Police District in New Zealand (July 2021)", y=1.04)
 plt.tick_params(
     axis="both",        # affect both the X and Y
@@ -25,6 +30,7 @@ plt.tick_params(
 plt.axis("off")
 plt.subplots_adjust(right=0.85)
 
+# Save and show the map
 plt.savefig("nz_firearm_licenses.png", dpi=300)
 plt.show()
 
